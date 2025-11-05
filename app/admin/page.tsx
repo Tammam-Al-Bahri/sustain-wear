3c2f865dbe41db90aa5bec1596dc06910b9775f2
Harrun01 <harrunhanduleh04@gmail.com>
2025-11-05 15:30:35 +0000

    feat: enhance admin page with navigation link and button for setting user permissions

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,14 @@
-export default function Admin () {
-    return <div>Admin</div>
+"use client"
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+export default function Admin() {
+  return (
+    <div>
+      <h1>Admin</h1>
+
+      <Link href="/admin/set-user-role">
+        <Button>  Set Permissions </Button>
+      </Link>
+    </div>
+  )
 }
