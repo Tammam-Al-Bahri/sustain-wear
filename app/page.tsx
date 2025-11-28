0a4fc9ff2caa1c5083112f4170d6423e84154bab
TyreseF06 <c4009423@hallam.shu.ac.uk>
2025-11-28 14:17:20 +0000

    added filler messages ot the dashcard component

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,20 @@ import { DashCard } from "@/components/ui/dashcard";
 export default function Home() {
     return (
         <main>
-            <div className="flex justify-center w-full">
-                <div className="flex flex-col gap-4">
-                    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
-                        <DashCard
-                        title="Total Consumption"
-                        value={`500 kWh`}
-                        />
-                        <DashCard
-                        title="Total Active Connections"
-                        value={`$100`}
-                        />
-                        <DashCard title="Total Active Connections" value={`100`} />
-                    </div>
+            <div className="flex flex-col gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                    <DashCard                        
+                    title="Total Consumption"
+                    value={`500 kWh`}
+                    />
+                    <DashCard
+                    title="Total Active Connections"
+                    value={`$100`}
+                    />
+                    <DashCard 
+                    title="Total Active Connections" 
+                    value={`100`} 
+                    />
                 </div>
             </div>
         </main>
