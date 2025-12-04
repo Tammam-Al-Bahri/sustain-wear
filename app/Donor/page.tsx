f5c100131cfb59913c85931c05eb07a01b73afbd
TyreseF06 <c4009423@hallam.shu.ac.uk>
2025-12-04 16:32:30 +0000

    moved the cards to the donor page

--- app/Donor/page.tsx
+++ app/Donor/page.tsx
@@ -1,16 +1,15 @@
 import { Card } from "@/components/ui/card"
 import { currentUser } from "@clerk/nextjs/server";
+import ClientPage from "../page-client";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SustainWear Donor",
+  description: "Donor Home Page",
+};
 
 export default async function Donor() {
   const user = await currentUser();
-  return (
-    <div>
-      <div className="w-screen flex justify-center">
-        <p id="welcome-message">Welcome, {user?.firstName}</p>
-      </div>    
-    </div>
+  return (<ClientPage />
   );
 }
-
-
-
