0798cf308c11a14c4ab3a560ca3b28ba430cce7b
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2025-12-09 16:57:49 +0000

    fix: checkUser early return feat: update .env.example

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -18,9 +18,6 @@ export const checkUser = async () => {
     if (loggedInUser) {
         return loggedInUser;
     }
-    if (!loggedInUser) {
-        return null;
-    }
 
     // if not in db, create new user
     const newUser = await prisma.user.create({
