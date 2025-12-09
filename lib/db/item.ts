ba627aa4575affbaec1785157db22fb0b1039119
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2025-12-09 04:17:04 +0000

    Add mass and material fields to item creation process
    - Updated createItemAction to include mass and material parameters.
    - Modified CreateItemForm to capture mass and material inputs.
    - Enhanced createItem function to handle new fields in the database.
    - Updated Prisma schema to define mass and material for Item model.

--- lib/db/item.ts
+++ lib/db/item.ts
@@ -1,10 +1,19 @@
-import { ItemCategory, ItemCondition, ItemSize, ItemStatus, ItemType } from "@prisma/client";
+import {
+    ItemCategory,
+    ItemCondition,
+    ItemMaterial,
+    ItemSize,
+    ItemStatus,
+    ItemType,
+} from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 
 export async function createItem(
     userId: string,
     name: string,
     description: string,
+    mass: number,
+    material: ItemMaterial,
     size: ItemSize,
     type: ItemType,
     category: ItemCategory,
@@ -12,12 +21,15 @@ export async function createItem(
     status: ItemStatus,
     imageUrls?: string[]
 ) {
+    if (mass < 0) mass === 0;
     return await prisma.item.create({
         data: {
             userId,
             imageUrls,
             name,
             description,
+            mass,
+            material,
             size,
             type,
             category,
