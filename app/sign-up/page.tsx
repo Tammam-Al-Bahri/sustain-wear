0fbe89f7d5b89b319300b60b87826c7b164b24a7
Obada Bitar <c4013337@hallam.shu.ac.uk>
2025-12-14 15:50:17 +0000

    refactor: enhance layout structure and improve responsiveness for sign-in and sign-up pages

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -4,8 +4,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <div className="signup-container">
-      <div className="left-section">
+    <div className="signup-container flex! sm:grid!">
+      <div className="left-section hidden! sm:flex!">
         <div className="brand-box">
           {/* <-- This is for our future logo--> */}
           <h1 className="brand-title">SustainWear</h1>
@@ -13,22 +13,24 @@ export default function SignUpPage() {
         </div>
       </div>
 
-      <div className="right-section">
-        <SignUp
-          appearance={{
-            elements: {
-              formButtonPrimary: "clerk-primary-btn",
-              card: "clerk-card",
-              headerTitle: "clerk-title",
-              headerSubtitle: "clerk-subtext",
-              formFieldInput: "clerk-input",
-              footer: "clerk-footer",
-            },
-            layout: {
-              socialButtonsPlacement: "bottom",
-            },
-          }}
-        />
+      <div className="right-section w-full">
+        <div className="flex justify-center items-center h-full p-10">
+          <SignUp
+            appearance={{
+              elements: {
+                formButtonPrimary: "clerk-primary-btn",
+                card: "clerk-card",
+                headerTitle: "clerk-title",
+                headerSubtitle: "clerk-subtext",
+                formFieldInput: "clerk-input",
+                footer: "clerk-footer",
+              },
+              layout: {
+                socialButtonsPlacement: "bottom",
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
