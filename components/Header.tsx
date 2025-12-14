55d658d8c03914eed1d99ead5ee0f3261b506a8f
Obada Bitar <c4013337@hallam.shu.ac.uk>
2025-12-14 11:26:15 +0000

    refactor: update header component for responsive dropdown menu and clean up styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,12 @@ import { Button } from "@/components/ui/button";
 import { checkUser } from "@/lib/checkUser";
 import DonationHeaderButton from "./donation/DonationHeaderButton";
 import UserStatButton from "./UserStatButton";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/headerDropDown";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/headerDropDown";
 
 export default async function Header() {
   const user = await checkUser();
@@ -21,7 +26,8 @@ export default async function Header() {
       {/* Right side (sign buttons / user) */}
       <div className="flex items-center gap-4">
         <SignedOut>
-          <div className="flex items-center gap-3">
+          {/* Large screens: show buttons inline */}
+          <div className="hidden md:flex items-center gap-3">
             <Link href="/sign-in">
               <Button
                 variant="outline"
@@ -37,36 +43,56 @@ export default async function Header() {
               </Button>
             </Link>
           </div>
+
+          {/* Small screens: show dropdown */}
+          <div className="md:hidden">
+            <DropdownMenu modal={false}>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">Menu</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>
+                  <Link href="/sign-in" className="w-full">
+                    Log in
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/sign-up" className="w-full">
+                    Sign Up
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </SignedOut>
 
-                 <SignedIn>
-                    {/* Large screens: show buttons inline */}
-                    <div className="hidden md:flex gap-4">
-                        <DonationHeaderButton />
-                        <UserStatButton />
-                        <UserButton afterSignOutUrl="/" />
-                    </div>
+        <SignedIn>
+          {/* Large screens: show buttons inline */}
+          <div className="hidden md:flex gap-4">
+            <DonationHeaderButton />
+            <UserStatButton />
+            <UserButton afterSignOutUrl="/" />
+          </div>
 
-                    {/* Small screens: show dropdown */}
-                    <div className="md:hidden">
-                        <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="outline">Menu</Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuItem>
-                            <DonationHeaderButton />
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                            <UserStatButton />
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                        </DropdownMenu>
-                        <UserButton afterSignOutUrl="/" />
-                    </div>
-                </SignedIn>
-
-            </div>
-        </header>
-    );
+          {/* Small screens: show dropdown */}
+          <div className="md:hidden">
+            <DropdownMenu modal={false}>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">Menu</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>
+                  <DonationHeaderButton />
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <UserStatButton />
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </SignedIn>
+      </div>
+    </header>
+  );
 }
