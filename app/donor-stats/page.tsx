0198256fbbb665096368f3f0ab88818bdccad3d3
Obada Bitar <c4013337@hallam.shu.ac.uk>
2025-12-15 18:52:00 +0000

    refactor: Update donor stats layout for improved readability and design consistency

--- app/donor-stats/page.tsx
+++ app/donor-stats/page.tsx
@@ -24,22 +24,29 @@ export default function donorStats () {
 
     return(
         <>
-            <div className="flex p-4 justify-center items-start text-2xl font-bold">
-                <h1>Your Stats</h1>
-            </div>
-            <div className="flex p-4 justify-center items-start">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-                    <DashCard
-                        title="Total Donations"
-                        value={donationCounts.userTotal !== null ? String(donationCounts.userTotal) : "Loading..."}
-                    />
-                    <DashCard
-                        title="Total CO2 emmissions saved"
-                        value={userTotal !== null ? String(userTotal) + " kg CO₂" : "Loading..."}
-                    />
+            <section className="flex justify-left w-full rounded-t-[15px] p-4 pb-[15px] pt-2.5 bg-linear-to-r from-[#C9EFC2] to-[#B7D5B2] shadow-[inset_0_-8px_0_0_rgba(58,150,46,0.2),inset_0_0_0_10px_rgba(255,255,255,0.15)] bg-clip-padding">
+                <h2 className="text-[23px] font-extrabold text-[#274D22]">
+                    Your Donation Stats
+                </h2>
+            </section>
+            <section className="flex flex-col p-6 w-full  gap-[20px]">
+                <div className="flex justify-center">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 w-full max-w-2xl">
+                        <DashCard
+                            title="Total Donations"
+                            value={donationCounts.userTotal !== null ? String(donationCounts.userTotal) : "Loading..."}
+                        />
+                        <DashCard
+                            title="Total CO₂ Emissions Saved"
+                            value={userTotal !== null ? String(userTotal) + " kg CO₂" : "Loading..."}
+                        />
+                    </div>
                 </div>
-                <br />
-            </div>
+                <div className="bg-[#EDFFEA] rounded-[25px] p-6 border-4 border-[#83B47D] shadow-none">
+                    <h3 className="text-[20px] font-bold text-[#274D22] mb-4">Donation History</h3>
+                    <DonationTable />
+                </div>
+            </section>
         </>
     )
 }
